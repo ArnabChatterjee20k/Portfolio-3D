dd86c3bb7c75291077f0fbc7a78b8ed98b9e8cdb
arnab <arnabchatterjee.ac.2@gmail.com>
2023-03-14 22:01:18 +0530

    Added about section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import Tilt from 'react-tilt'
 import { motion } from "framer-motion"
 
 import { styles } from "../styles"
 import { services } from "../constants"
 import { fadeIn, textVariant } from "../utils/motion"
-import { text } from "../constants/textData"
 import Markdown from '../components/markdown/Markdown'
 import useMarkdown from '../services/markdown/useMarkdown'
+import ServiceCard from '../components/about/ServiceCard'
 
 const About = () => {
-  const {data} = useMarkdown("./data/about.md") //targeting the public directory
+  const { data } = useMarkdown("./data/about.md") //targeting the public directory
 
   return (
     <>
@@ -21,8 +20,15 @@ const About = () => {
       <motion.p
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
         variants={fadeIn("", "", 0.1, 1)}>
-        <Markdown text={data}/>
+        <Markdown text={data} />
       </motion.p>
+        <div className='mt-20 flex flex-wrap gap-10'>
+          {
+            services.map((service,index)=>{
+              return <ServiceCard key={service.title} index={index} {...service}/>
+            })
+          }
+        </div>
     </>
   )
 }
